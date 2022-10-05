0499b8b180dfb081055cfcf3c0b67659039b406e
Eric Moser <emoser91@gmail.com>
2022-10-05 17:07:44 -0500

    finished ds3231 project

--- DS3231RealTimeClockI2C/src/main.cpp
+++ DS3231RealTimeClockI2C/src/main.cpp
@@ -1,344 +1,268 @@
 /* This project will interface a DS3231 Real Time Clock Module with the Arduino Uno
 
-  DS3231 Wiring
+  The I2C in this project was implemented a little different from the LCD Backpack project.
+  The I2C communications code works the same as LCD Backpack just less abstract functions.
+  This project could be updated to use the base I2C functions as LCD Backpack project.
 
-  Note, my first I2C Backpack fried itself so be careful wiring up
+  Read through the Datasheet attached in the folder for more information
+
+  DS3231 Wiring:
+  DAT(Data) to SDA/PC4/A4 Pin
+  CLK(Clock) to SCL/PB5/A5 Pin
+  5v to 5v Pin
+  GND to GND Pin
 
   Order of Operations:
+  There is no fancy bit setting required for this project like LCD backpack needed.
+  Below is the exact operation we are doing to write/read to/from the DS3231
+  The following operations will be the same for most I2C systems
+  We can get away with continued writes and reads because the data we want is next to
+  eachother in the DS3231 addresses.
+  The Write/Read timing diagram in the datasheet shows exactly how to commuicate with the DS3231
 
+  Write:
+  1. Send Start bit
+     Check return status
+  2. Send DS3231 Address containing 0 for write bit
+     Check return status
+  3. Send DS3231 Memory address we want to write to
+     Check return status
+  4. Send Seconds Data we want to write
+     Check return status
+  5. Send Minutes Data we want to write
+     Check return status
+  6. Send Hours data we want to write
+     Check return status
+  7. Send Stop bit
 
-
-  A wiring diagram is included in the file structure.
+  Read:  
+  1. Send Start bit
+     Check return status
+  2. Send DS3231 Address containing 1 for read bit
+     Check return status
+  3. Send DS3231 Memory address we want to read from
+     Check return status
+  4. Send Stop bit
+  5. Send Start bit
+     Check return status
+  6. Send DS3231 Address containing 1 for read bit
+     Check return status
+  7. Read Data buffer for Seconds
+     Send Ack
+  8. Read Data buffer for Minutes
+     Send Ack
+  9. Read Data buffer for Hours
+     Send Nack
+  10. Send Stop bit
 */
 #include <avr/io.h>
 #include <inttypes.h>
 #include <stdlib.h>
+#include <util/delay.h>
+#include <util/twi.h>
 
 #include <MSOE/delay.c>
 #include <MSOE/bit.c>
 #include <MSOE/lcd.c>
 
 // #include <Arduino.h> //Probably dont need, just for VS Code/Plateform IO
-#include <compat/twi.h> //WinAVR Library in avr/include/util/twi.h
 
+//https://www.avrfreaks.net/forum/ds3231-interfacing-atmega328p#comment-3046406
 
-//TWI Function Prototypes
-unsigned char  i2c_transmit(unsigned char type);
-char i2c_start(unsigned int dev_id, unsigned int dev_addr, unsigned char rw_type);
-void i2c_stop(void);
-char i2c_write(char data);
-char i2c_read(char *data,char ack_type);
-void Write_DS3231(unsigned char reg_addr,unsigned char data);
-unsigned char Read_DS3231(unsigned char reg_addr);
-void i2c_init(void);
+/*All Macro Definitions*/
+#define FOSC 16000000
+#define FREQ_SCL 400000
+//Arduino IDE Example I2C Scanner says address is 0x68 because that is the 7bits and 
+//does not take into account the read/write bit.
+#define RTCADDR 208 //Datasheet shows address is 1101000X where X is 0 for write and 1 for read
+#define RTCADDR_W 208
+#define RTCADDR_R 209
 
-// Function prototypes for data processing
-void Set_Time_DS3231(uint8_t seconds, uint8_t minutes, uint8_t hours);
-void Set_Date_DS3231(uint8_t day, uint8_t month, uint8_t year);
-
-void Read_Time_DS3231(uint8_t *seconds, uint8_t *minutes, uint8_t *hours);
-void Read_Date_DS3231(uint8_t *day, uint8_t *month, uint8_t *year);
-
-//////////////////////////////////
-/* SETUP FOR DS3231 */
-//////////////////////////////////
-#define SetBit(a, b) (a) |= (1 << (b))
-#define ClearBit(a, b) (a) &= ~(1 << (b))
-
-//DS3231 SPI/I2C LCD backpack driver
-#define DS3231_ID    0x68  // DS3231 Device Identifier 68
-#define DS3231_ADDR  0x00  // DS3231 Device Address
-
-//DS3231 Register Addresses
-#define DS3231_SECONDS  0x00
-#define DS3231_MINUTES  0x01
-#define DS3231_HOURS    0x02
-#define DS3231_DAY      0x03
-#define DS3231_DATE     0x04
-#define DS3231_MONTH    0x05
-#define DS3231_YEAR     0x06
-
-//I2C Status
-#define I2C_START 0
-#define I2C_DATA 1
-#define I2C_DATA_ACK 2
-#define I2C_STOP 3
-#define ACK 1
-#define NACK 0
-
-#define MAX_TRIES 50
-#define DATASIZE 32 //CHECK THIS
-
-
-int main (void)
+struct TIME
 {
-	//Initialize LCD Screen
+  unsigned char hh;
+  unsigned char mm;
+  unsigned char ss;
+};
+
+/*TWI Function Prototypes*/
+void TWI_Init(void);
+void TWI_Stop(void);
+unsigned char TWI_Start(unsigned char);
+unsigned char TWI_Write(unsigned char);
+unsigned char TWI_Read_Ack(void);
+unsigned char TWI_Read_Nack(void);
+
+/*RTC Function Prototypes*/
+void RTC_Time_Set(unsigned char, unsigned char, unsigned char);
+void RTC_Time_Get(struct TIME*);
+
+int main()
+{
+  //Initialize LCD Screen
 	lcd_init();
 	lcd_clear();
 	lcd_home();
-  
-  lcd_printf("X");
-  // lcd_printf("Real Time Clock");
-  /* Write Current Time */
-  /* You only need to set the time once, it will stay set even after resets */
-  /* You can run the code once to set and then reprogram Arduino with this section commented out */
-  uint8_t setSeconds = 0x00;
-  uint8_t setMinutes = 0x00;
-  uint8_t setHours = 0x00;
 
-  // Set_Time_DS3231(setSeconds, setMinutes, setHours);
+	struct TIME timeObj;
 
-  /* Read Current Time */
-  uint8_t readSeconds;
-  uint8_t readMinutes;
-  uint8_t readHours;
+	TWI_Init();
 
-  // Read_Time_DS3231(&readSeconds, &readMinutes, &readHours);
+  /* Setting Time Values */
+	timeObj.ss=0;  //8bit value: Upper 4bits are 10s place, lower 4bits are 1s place
+	timeObj.mm=3;  //8bit value: Upper 4bits are 10s place, lower 4bits are 1s place
+	timeObj.hh=22; //8bit value: bit7=0, bit6=0 for 24hour mode, bit5 is for 20, bit4 is for 10, lower 4 bits(bit3-bit0) are 1s place
 
-  //Write Seconds
-	Write_DS3231(DS3231_SECONDS,setSeconds);
-	delay_ms(1); //Wait for data to be processed
+  /* Command to set the time */
+  /* !!! Only needs to happen once then you can comment out the following line and load the code again !!! */
+	RTC_Time_Set(timeObj.hh,timeObj.mm,timeObj.ss);
 
-  // readSeconds = Read_DS3231(DS3231_SECONDS);
+  while(1)
+  {
+    RTC_Time_Get(&timeObj);
 
-	while(1)
-	{
-    // Read_Time_DS3231(&readSeconds, &readMinutes, &readHours);
+    lcd_home();
 
-    // readSeconds = Read_DS3231(DS3231_SECONDS);
+    if(timeObj.hh >= 10)
+    {
+      lcd_printf("%dh",timeObj.hh);
+    }
+    else
+    {
+      lcd_printf("0%dh",timeObj.hh);
+    }
 
-    //Print Seconds for Debugging
-    lcd_goto_xy(0,1);
-    lcd_printf("s: %d",setSeconds);
-	}
+    if(timeObj.mm >= 10)
+    {
+      lcd_printf("%dm",timeObj.mm);
+    }
+    else
+    {
+      lcd_printf("0%dm",timeObj.mm);
+    }
 
-	return 0;
+    if(timeObj.ss >= 10)
+    {
+      lcd_printf("%ds",timeObj.ss);
+    }
+    else
+    {
+      lcd_printf("0%ds",timeObj.ss);
+    }
+   
+  }
+
+  return 0;
 }
-
-/////////////////////////////////////
-/* Two Wire Interface (TWI) : I2C  */
-/////////////////////////////////////
-void i2c_init(void)
+void TWI_Init(void)
 {
   // Initial ATMega328P TWI/I2C Peripheral
-  TWSR = 0x00; // TWI Status Register: Select Prescaler of 1 (TWPS1=0, TWPS0=0)
-               // Also holds status codes and we are clearing all
+  // TWI Status Register: Select Prescaler of 1 (TWPS1=0, TWPS0=0)
+  // Also holds status codes and we are clearing all
+  TWSR = 0x00; 
 
   // Setting SCL (Serial clock) which is controlled by setting TWI Bit Rate Register
   // SCL frequency = (Cpu Clock Frequency) / ( 16 + 2(TWBR) * Prescaler Value)
   // SCL frequency = 16000000 / (16 + 2 * 12 * 1) = 400 kHz
-  // LCD Backpack MCP23008 Datasheet asks for 100 khz, 400 khz or 1.7 Mhz: we choose to use 400 khz
-  TWBR = 0x0C; // 12 Decimal
+  // DS3231 Datasheet asks for 400 khz
+  TWBR = ((FOSC / FREQ_SCL) - 16) / 2;
+
+  /* Send Start Condition */
+  TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
 }
 
-/* START I2C Routine */
-unsigned char i2c_transmit(unsigned char type) 
+unsigned char TWI_Start(unsigned char addr)
 {
-	switch(type) {
-		//Each of the following cases are clearly outlined in the datasheet
-		//Datasheet outlines what bits to set for specific cases
-		case I2C_START:    // Send Start Condition
-			TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN); //TWCR: TWI Control Register
-			break;
-		case I2C_DATA:     // Send Data with No-Acknowledge
-			TWCR = (1 << TWINT) | (1 << TWEN);
-			break;
-		case I2C_DATA_ACK: // Send Data with Acknowledge
-			TWCR = (1 << TWEA) | (1 << TWINT) | (1 << TWEN);
-			break;
-		case I2C_STOP:     // Send Stop Condition
-			TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+  unsigned char twst;
 
-		return 0;
-  }
-
-lcd_printf("B");
-  // Wait for TWINT flag set on Register TWCR
+  /* Send Start Condition */
+  TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
   while (!(TWCR & (1 << TWINT)));
- lcd_printf("C");
-  // Return TWI Status Register, mask off the prescaler bits (TWPS1,TWPS0)
-  return (TWSR & 0xF8);
+  /* Check return status */
+  twst = TW_STATUS & 0xF8; // Return TWI Status Register, mask off the prescaler bits (TWPS1,TWPS0)
+  if ( (twst != TW_START) && (twst != TW_REP_START)) return 1;
 
+  /* Send Data Containing Device Address */
+  TWDR = addr;
+  TWCR = (1 << TWINT) | (1 << TWEN);
+  while (!(TWCR & (1 << TWINT)));
+  /* Check return status */
+  twst = TW_STATUS & 0xF8; // Return TWI Status Register, mask off the prescaler bits (TWPS1,TWPS0)
+  if ( (twst != TW_MT_SLA_ACK) && (twst != TW_MR_SLA_ACK) ) return 1;
+
+  return 0;
 }
 
-// Function that sends start condition and slave address
-// Function will return 0 if executed properly and -1 if it fails
-char i2c_start(unsigned int dev_id, unsigned int dev_addr, unsigned char rw_type)
+void TWI_Stop(void)
 {
-	//This function uses labels and goto statements
-	unsigned char n = 0;
-	unsigned char twi_status;
-	char r_val = -1;
-
-	i2c_retry:
-		if (n++ >= MAX_TRIES) return r_val;
-
-		// Transmit Start Condition
-		twi_status = i2c_transmit(I2C_START);
-  // lcd_print_uint8(twi_status);
-		// Check the TWI Status
-		if (twi_status == TW_MT_ARB_LOST) goto i2c_retry;
-		if ((twi_status != TW_START) && (twi_status != TW_REP_START)) goto i2c_quit; //bad status recieved
-
-		// Load slave address (SLA_W)
-		// TWDR = (dev_id & 0xF0) | (dev_addr & 0x0E) | rw_type; //Filling TWI Data Register for LCD Backpack
-		TWDR = (dev_id & 0xFE) | rw_type; //Filling TWI Data Register
-
-
-		// Transmit I2C Data
-		twi_status = i2c_transmit(I2C_DATA);
-lcd_print_uint8(twi_status);
-		// Check the TWSR status
-		if ((twi_status == TW_MT_SLA_NACK) || (twi_status == TW_MT_ARB_LOST)) goto i2c_retry;
-		if (twi_status != TW_MT_SLA_ACK) goto i2c_quit; //bad status recieved
-
-		r_val = 0;
-
-	i2c_quit:
-		return r_val;
+  /* Send Stop Condition */
+  TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+  while (TWCR & (1 << TWSTO));
 }
 
-// Function will send stop condition
-void i2c_stop(void)
+unsigned char TWI_Write(unsigned char byte_data)
 {
-  unsigned char twi_status;
+  unsigned char twst;
 
-  // Transmit I2C Data
-  twi_status = i2c_transmit(I2C_STOP);
+  /* Send Data */
+  TWDR = byte_data;
+  TWCR = (1 << TWINT) | (1 << TWEN);
+  while (!(TWCR & (1 << TWINT)));
+  /* Check return status */
+  twst = TW_STATUS & 0xF8; // Return TWI Status Register, mask off the prescaler bits (TWPS1,TWPS0)
+  if ( twst != TW_MT_DATA_ACK) return 1;
+
+  return 0;
 }
 
-// Function will fill TWI data register and then send the data
-// Function will return 0 if executed properly or -1 if it fails
-char i2c_write(char data)
+unsigned char TWI_Read_Ack(void)
 {
-  unsigned char twi_status;
-  char r_val = -1;
+  /* Send Read Ack */
+  TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
+  while (!(TWCR & (1 << TWINT)));
 
-  // Send the Data to I2C Bus
-  TWDR = data;
-
-  // Transmit I2C Data
-  twi_status = i2c_transmit(I2C_DATA);
-
-  // Check the TWSR status
-  if (twi_status != TW_MT_DATA_ACK) goto i2c_quit; //bad status recieved
-
-  r_val = 0;
-
-	i2c_quit:
-  return r_val;
+  return TWDR;
 }
 
-// Function will fill "data" varriable with the TWI Data register data and Send Acknowledge or No Acknowledge based on input
-// Function will return 0 if executed properly or -1 if it fails
-char i2c_read(char *data,char ack_type)
+unsigned char TWI_Read_Nack(void)
 {
-  unsigned char twi_status;
-  char r_val = -1;
+  /* Send Read Nack */
+  TWCR = (1 << TWINT) | (1 << TWEN);
+  while (!(TWCR & (1 << TWINT)));
 
-  if (ack_type) 
-  {
-    // Read I2C Data and Send Acknowledge
-    twi_status = i2c_transmit(I2C_DATA_ACK);
-
-    if (twi_status != TW_MR_DATA_ACK) goto i2c_quit; //bad status recieved
-  } 
-  else 
-  {
-    // Read I2C Data and Send No Acknowledge
-    twi_status = i2c_transmit(I2C_DATA);
-
-    if (twi_status != TW_MR_DATA_NACK) goto i2c_quit; //bad status recieved
-  }
-
-  // Get the Data
-  *data = TWDR;
-
-  r_val = 0;
-
-  i2c_quit:
-  return r_val;
+  return TWDR;
 }
 
-// Write data to DS3231
-void Write_DS3231(unsigned char reg_addr,unsigned char data)
+/*RTC Function Definitions*/
+void RTC_Time_Set(unsigned char hh, unsigned char mm, unsigned char ss)
 {
-  int8_t test;
-  lcd_printf("A");
-   // Start the I2C Write Transmission
-   test = i2c_start(DS3231_ID,DS3231_ADDR,TW_WRITE);
-lcd_print_int8(test);
-   // Sending the Register Address
-   i2c_write(reg_addr);
-lcd_printf("2");
-   // Write data to DS3231 Register
-   i2c_write(data);
-lcd_printf("3");
-   // Stop I2C Transmission
-   i2c_stop();
-   lcd_printf("4");
+  unsigned char memoryStartAddr = 0; //Start address for where seconds are held
+
+  TWI_Start(RTCADDR_W);
+  TWI_Write(memoryStartAddr);
+  TWI_Write(ss);
+  TWI_Write(mm);
+  TWI_Write(hh);
+  TWI_Stop();
 }
 
-// Read data from DS3231
-unsigned char Read_DS3231(unsigned char reg_addr)
+void RTC_Time_Get(struct TIME * timePtr)
 {
-   char data;
+  unsigned char memoryStartAddr = 0; //Start address for where seconds are held
 
-   // Start the I2C Write Transmission
-   i2c_start(DS3231_ID,DS3231_ADDR,TW_WRITE);
+  TWI_Start(RTCADDR_W);
+  TWI_Write(memoryStartAddr);
+  TWI_Stop();                       //Timing diagram doesnt show a stop but all examples I see has it
 
-   // Read data from DS3231 Register Address
-   i2c_write(reg_addr);
+  TWI_Start(RTCADDR_R);
+  timePtr->ss=TWI_Read_Ack();
+  timePtr->mm=TWI_Read_Ack();
+  timePtr->hh=TWI_Read_Nack();      //Datasheet wants a Nack at the end
+  TWI_Stop();
 
-   // Stop I2C Transmission
-   i2c_stop();
-
-   // Re-Start the I2C Read Transmission
-   i2c_start(DS3231_ID,DS3231_ADDR,TW_READ);
-
-   i2c_read(&data,NACK);
-
-   // Stop I2C Transmission
-   i2c_stop();
-
-   return data;
-}
-
-
-///////////////////////////////////////
-/* DS3221 Data Processing Functions  */
-///////////////////////////////////////
-void Set_Time_DS3231(uint8_t seconds, uint8_t minutes, uint8_t hours)
-{
-  //Write Seconds
-	Write_DS3231(DS3231_SECONDS,seconds);
-	delay_ms(1); //Wait for data to be processed
-
-  //Write Minutes
-	Write_DS3231(DS3231_MINUTES,minutes);
-	delay_ms(1); //Wait for data to be processed
-
-  //Write Hours
-	Write_DS3231(DS3231_HOURS,hours);
-	delay_ms(1); //Wait for data to be processed
-}
-
-void Set_Date_DS3231(uint8_t day, uint8_t month, uint8_t year)
-{
-
-}
-
-void Read_Time_DS3231(uint8_t *seconds, uint8_t *minutes, uint8_t *hours)
-{
-
-  *seconds = Read_DS3231(DS3231_SECONDS);
-
-  *minutes = Read_DS3231(DS3231_MINUTES);
-
-  *hours = Read_DS3231(DS3231_HOURS);
-}
-
-void Read_Date_DS3231(uint8_t *day, uint8_t *month, uint8_t *year)
-{
-
+  /* Process Data */
+  timePtr->ss= (((timePtr->ss & 0xF0) >> 4) * 10 ) + (timePtr->ss & 0x0F); //Upper 4bits are 10s place, lower 4bits are 1s place
+  timePtr->mm= (((timePtr->mm & 0xF0) >> 4) * 10 ) + (timePtr->mm & 0x0F); //Upper 4bits are 10s place, lower 4bits are 1s place
+  /* Using 24hour setting where hour bit6=0 */
+  timePtr->hh= (((timePtr->hh & 0x20) >> 4) * 20 ) + (((timePtr->hh & 0x10) >> 4) * 10 ) + (timePtr->hh & 0x0F); //Bit 5 is for 20, bit 4 is for 10, lower 4 bits are 1s place
 }
